38ad7f9f65b8062b362f693e255a02177eb8dec1
jchans <jiangchenghans@gmail.com>
2021-11-16 02:07:50 +0800

    Adjust link and button style

--- src/NavBike.js
+++ src/NavBike.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./NavBike.css";
@@ -21,13 +21,20 @@ function NavBike() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link href="#home">
-                  <span className="nav-text">Home</span>
+                <Nav.Link href="#nav-route-plan">
+                  <div className="nav-text">規劃路線</div>
                 </Nav.Link>
-                <Nav.Link href="#link">
-                  <span className="nav-text">Link</span>
+                <Nav.Link href="#nav-youbike-map">
+                  <div className="nav-text">YouBike地圖</div>
                 </Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <Nav.Link href="#nav-bike-routes">
+                  <div className="nav-text">自行車路線</div>
+                </Nav.Link>
+                <Nav.Link href="#nav-login-button">
+                  <div className="nav-text login-button">登入</div>
+                </Nav.Link>
+
+                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                   <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">
                     Another action
@@ -39,7 +46,7 @@ function NavBike() {
                   <NavDropdown.Item href="#action/3.4">
                     Separated link
                   </NavDropdown.Item>
-                </NavDropdown>
+                </NavDropdown> */}
               </Nav>
             </Navbar.Collapse>
           </div>
