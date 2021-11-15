71b6cfebbae715f751f515af3a808479181b5eab
jchans <jiangchenghans@gmail.com>
2021-11-16 02:24:55 +0800

    Add plan route button

--- src/MainContent.js
+++ src/MainContent.js
@@ -9,6 +9,7 @@ function MainContent() {
         近年來因應健康、環保等開啟了騎自行車風潮，我們整理了各縣市的自行車路線，結合
         Youbike 讓您快速規劃您的活動。
       </p>
+      <div className="button-plan-route">規劃路線</div>
     </div>
   );
 }
