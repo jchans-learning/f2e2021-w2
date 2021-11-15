a56621c8b5c0f9e0ab8cd3fe0e31cde2a6ff7b54
jchans <jiangchenghans@gmail.com>
2021-11-16 02:34:43 +0800

    Adjust item margins

--- src/MainContent.js
+++ src/MainContent.js
@@ -5,10 +5,11 @@ function MainContent() {
   return (
     <div className="main-content">
       <h1>YouBike, YouRoad</h1>
-      <p className="intro-text">
+      <div className="intro-text">
         近年來因應健康、環保等開啟了騎自行車風潮，我們整理了各縣市的自行車路線，結合
         Youbike 讓您快速規劃您的活動。
-      </p>
+      </div>
+
       <div className="button-plan-route">規劃路線</div>
     </div>
   );
