4322057e4028a8f31a34ed84d97dbf6994ea72a4
jchans <jiangchenghans@gmail.com>
2021-11-17 15:59:30 +0800

    Add file: LandingPage.js

---
+++ src/LandingPage.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function LandingPage() {
+  return (
+    <div>
+      <div className="main-content">
+        <h1>YouBike, YouRoad</h1>
+        <div className="intro-text">
+          近年來因應健康、環保等開啟了騎自行車風潮，我們整理了各縣市的自行車路線，結合
+          Youbike 讓您快速規劃您的活動。
+        </div>
+
+        <div className="button-plan-route">規劃路線</div>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
--- src/MainContent.js
+++ src/MainContent.js
@@ -1,16 +1,11 @@
 import React from "react";
+import LandingPage from "./LandingPage";
 import "./MainContent.css";
 
 function MainContent() {
   return (
-    <div className="main-content">
-      <h1>YouBike, YouRoad</h1>
-      <div className="intro-text">
-        近年來因應健康、環保等開啟了騎自行車風潮，我們整理了各縣市的自行車路線，結合
-        Youbike 讓您快速規劃您的活動。
-      </div>
-
-      <div className="button-plan-route">規劃路線</div>
+    <div>
+      <LandingPage />
     </div>
   );
 }
