85ebbe51d896a3c662f4432b31e9b5fa56bde459
jchans <jiangchenghans@gmail.com>
2021-11-18 15:12:31 +0800

    Move LandingPage.js to components folder

--- src/MainContent.js
+++ src/MainContent.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import LandingPage from "./LandingPage";
+import LandingPage from "./components/LandingPage";
 import "./MainContent.css";
 
 function MainContent() {
