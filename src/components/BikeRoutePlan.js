50a49c74010fb4a25a306581ca7972c58237c261
jchans <jiangchenghans@gmail.com>
2021-11-18 16:02:33 +0800

    Add animation when component appears

--- src/components/BikeRoutePlan.js
+++ src/components/BikeRoutePlan.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./styles/BikeRoutePlan.css";
 
 function BikeRoutePlan() {
   return (
-    <div>
+    <div className="bike-route-plan">
       <div className="main-content">
-        <h1>Bile Route Plan</h1>
+        <h1>Bike Route Plan</h1>
         <div className="intro-text">路線計畫畫面待處理。</div>
       </div>
     </div>
