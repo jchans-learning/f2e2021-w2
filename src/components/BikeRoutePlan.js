fdaa358c26c7238f6b7b7ccf8ff200f3b242fb54
jchans <jiangchenghans@gmail.com>
2021-11-18 15:36:21 +0800

    New Component: BikeRoutePlan

---
+++ src/components/BikeRoutePlan.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function BikeRoutePlan() {
+  return (
+    <div>
+      <div className="main-content">
+        <h1>Bile Route Plan</h1>
+        <div className="intro-text">路線計畫畫面待處理。</div>
+      </div>
+    </div>
+  );
+}
+
+export default BikeRoutePlan;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import LandingPage from "./LandingPage";
+import BikeRoutePlan from "./BikeRoutePlan";
 import "./styles/MainContent.css";
 
 function MainContent() {
@@ -12,7 +13,9 @@ function MainContent() {
 
   return (
     <div>
-      {hideLandingPage ? null : (
+      {hideLandingPage ? (
+        <BikeRoutePlan />
+      ) : (
         <LandingPage startPlanningIsClicked={startPlanning} />
       )}
     </div>
